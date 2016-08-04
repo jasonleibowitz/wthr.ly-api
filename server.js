849dbd4413b67a9426e71f95b7260ffc35f98398
Jason Leibowitz <jason@Leibowitz.me>
2016-08-04 17:46:52 -0400

    error handling

--- server.js
+++ server.js
@@ -6,25 +6,26 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-})
+});
+
+app.get('/', function(req, res) {
+  res.send('Please visit https://reserve.com');
+});
 
 app.get('/forecast/:apiKey/:coords', function(req, res) {
-  console.log('params:', req.params)
   var apiKey = req.params.apiKey;
   var coords = req.params.coords;
 
   request
     .get(`https://api.forecast.io/forecast/${ apiKey }/${ coords }`, function(err, response, body) {
-      if (response.statusCode === 200) {
-        console.log('Yay, it worked!');
-        res.send(response);
-      } else {
-        console.log('statusCode:', response.statusCode);
-        console.log('statusMessage:', response.statusMessage);
-        res.send(response);
-      }
+      if (err) res.send(err)
+      res.send(response);
     })
+});
 
+app.get('*', function(req, res) {
+  res.status(404);
+  res.send('That page doesn\'t exist. Pleast visit https://reserve.com');
 });
 
 app.listen(8080, function() {
